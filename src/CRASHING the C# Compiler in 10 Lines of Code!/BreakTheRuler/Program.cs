1b270730e984206e720bc23b2a784b3e5123786e
Ivaylo Kenov <ivaylo.kenov@gmail.com>
2019-11-21 21:15:48 +0200

    Added BreakTheRuler

---
+++ src/CRASHING the C# Compiler in 10 Lines of Code!/BreakTheRuler/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BreakTheRuler
+{
+    public class MyClass<T1, T2>
+    {
+        public MyClass<MyClass<T1, T2>, MyClass<T1, T2>> HitMe { get; }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var instance = new MyClass<string, string>();
+
+            var hitMe = instance
+                .HitMe.HitMe.HitMe
+                .HitMe.HitMe.HitMe
+                .HitMe.HitMe.HitMe
+                .HitMe.HitMe.HitMe
+                .HitMe.HitMe.HitMe
+                .HitMe.HitMe.HitMe
+                .HitMe.HitMe.HitMe
+                .HitMe.HitMe.HitMe
+                .HitMe.HitMe.HitMe;
+        }
+    }
+}
