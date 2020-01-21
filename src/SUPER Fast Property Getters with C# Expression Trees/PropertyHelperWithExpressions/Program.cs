75f421acf05e6b13d1ccfec193c7e16b4c62622f
ivaylokenov <ivaylo.kenov@gmail.com>
2020-01-21 10:52:02 +0200

    Added SUPER Fast Property Getters with C# Expression Trees source code

---
+++ src/SUPER Fast Property Getters with C# Expression Trees/PropertyHelperWithExpressions/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace PropertyHelperWithExpressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            // return RedirectToAction("Index", new { id = 5, query = "Text" });
+            // Dictionary { ["id"] = 5, ["query"] = "Text" }
+
+            var obj = new { id = 5, query = "Text" };
+
+            var dict = new Dictionary<string, object>();
+
+            var stopwatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                PropertyHelper
+                    .GetProperties(obj.GetType())
+                    .Select(pr => new
+                    {
+                        pr.Name,
+                        Value = pr.Getter(obj)
+                    })
+                    .ToList()
+                    .ForEach(pr => dict[pr.Name] = pr.Value);
+            }
+
+            Console.WriteLine($"{stopwatch.Elapsed} - Normal Expression Tree");
+            Console.WriteLine(dict.Count);
+
+            dict = new Dictionary<string, object>();
+
+            stopwatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                PropertyHelperFast
+                    .GetProperties(obj.GetType())
+                    .Select(pr => new
+                    {
+                        pr.Name,
+                        Value = pr.Getter(obj)
+                    })
+                    .ToList()
+                    .ForEach(pr => dict[pr.Name] = pr.Value);
+            }
+
+            Console.WriteLine($"{stopwatch.Elapsed} - Fast Expression Tree");
+            Console.WriteLine(dict.Count);
+        }
+    }
+}
---
+++ src/SUPER Fast Property Getters with C# Expression Trees/PropertyHelperWithExpressions/PropertyHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace PropertyHelperWithExpressions
+{
+    public class PropertyHelper
+    {
+        private static readonly Type TypeOfObject = typeof(object);
+
+        private static readonly ConcurrentDictionary<Type, PropertyHelper[]> cache
+            = new ConcurrentDictionary<Type, PropertyHelper[]>();
+
+        public string Name { get; set; }
+
+        // Object obj => obj.Property;
+        public Func<object, object> Getter { get; set; }
+
+        public static PropertyHelper[] GetProperties(Type type)
+            => type
+                   .GetProperties()
+                   .Select(pr =>
+                   {
+                       // Object obj
+                       var parameter = Expression.Parameter(TypeOfObject, "obj");
+
+                       // (T)obj
+                       var parameterConvert = Expression.Convert(parameter, type);
+
+                       // ((T)obj).Property
+                       var body = Expression.MakeMemberAccess(parameterConvert, pr);
+
+                       // (object)((T)obj).Property
+                       var convertedBody = Expression.Convert(body, TypeOfObject);
+
+                       // Object obj => (object)((T)obj).Property
+                       var lambda = Expression
+                              .Lambda<Func<object, object>>(convertedBody, parameter);
+
+                       var propertyGetterFunc = lambda.Compile();
+
+                       return new PropertyHelper
+                       {
+                           Name = pr.Name,
+                           Getter = propertyGetterFunc
+                       };
+                   })
+                   .ToArray();
+    }
+}
