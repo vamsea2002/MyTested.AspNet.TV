838a9a472600cc8280ea84a3817c5710fe3a16dc
ivaylokenov <ivaylo.kenov@gmail.com>
2020-01-23 14:44:52 +0200

    Added Embracing NON-NULLABLE Reference Types in C# 8.0

---
+++ src/Embracing NON-NULLABLE Reference Types in C# 8.0/NullableReferenceTypes/Cat.cs
@@ -0,0 +1,14 @@
+﻿namespace NullableReferenceTypes
+{
+    public class Cat
+    {
+        public Cat(string name)
+        {
+            this.Name = name;
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ src/Embracing NON-NULLABLE Reference Types in C# 8.0/NullableReferenceTypes/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace NullableReferenceTypes
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    class Program
+    {
+        static void Main()
+        {
+            SomeGenericMethod<Cat>();
+        }
+
+        public static void SomeGenericMethod<TKey>()
+            where TKey : notnull
+        {
+        }
+
+        [return: NotNullIfNotNull("parameter")]
+        public static string? Test(string? parameter)
+        {
+            return parameter ?? "Test";
+        }
+
+        public static string? Test(bool returnNotNullIfTrue)
+        {
+            return returnNotNullIfTrue ? "NotNull" : null;
+        }
+    }
+}
